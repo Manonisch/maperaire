7266c1ab6a8285ee775a1d701420ee31bee577da
Manonisch <e.baudisch@mailbox.org>
2025-04-13 13:27:40 +0200

    stored

---
+++ src/data/querys/positions_test.ts
@@ -0,0 +1,129 @@
+export const Test = [
+  {
+    bookIndex: 0,
+    chapterIndex: 0,
+    matches: ["Will", "Will"],
+  },
+  {
+    bookIndex: 1,
+    chapterIndex: 1,
+    matches: ["Willoughby"],
+  },
+  {
+    bookIndex: 1,
+    chapterIndex: 2,
+    matches: ["Will", "Will", "Will"],
+  },
+  {
+    bookIndex: 1,
+    chapterIndex: 11,
+    matches: ["Will", "Will"],
+  },
+  {
+    bookIndex: 1,
+    chapterIndex: 13,
+    matches: [
+      "Willoughby",
+      "Will",
+      "Willoughby",
+      "Willoughby",
+      "Willoughby",
+      "Willoughby",
+    ],
+  },
+  {
+    bookIndex: 2,
+    chapterIndex: 9,
+    matches: ["Will", "Will"],
+  },
+  {
+    bookIndex: 2,
+    chapterIndex: 10,
+    matches: ["William"],
+  },
+  {
+    bookIndex: 2,
+    chapterIndex: 11,
+    matches: ["William"],
+  },
+  {
+    bookIndex: 3,
+    chapterIndex: 4,
+    matches: ["Will", "Will"],
+  },
+  {
+    bookIndex: 3,
+    chapterIndex: 5,
+    matches: ["Will"],
+  },
+  {
+    bookIndex: 4,
+    chapterIndex: 8,
+    matches: ["Will", "Will", "Will", "Will", "Will", "Will", "Will"],
+  },
+  {
+    bookIndex: 4,
+    chapterIndex: 9,
+    matches: ["William", "Will", "Will", "Will"],
+  },
+  {
+    bookIndex: 5,
+    chapterIndex: 1,
+    matches: ["Will"],
+  },
+  {
+    bookIndex: 5,
+    chapterIndex: 3,
+    matches: ["Will", "Will"],
+  },
+  {
+    bookIndex: 5,
+    chapterIndex: 13,
+    matches: ["William"],
+  },
+  {
+    bookIndex: 7,
+    chapterIndex: 2,
+    matches: ["Will"],
+  },
+  {
+    bookIndex: 7,
+    chapterIndex: 3,
+    matches: ["Will"],
+  },
+  {
+    bookIndex: 7,
+    chapterIndex: 12,
+    matches: ["Will", "Will"],
+  },
+  {
+    bookIndex: 7,
+    chapterIndex: 14,
+    matches: ["Will", "Will"],
+  },
+  {
+    bookIndex: 7,
+    chapterIndex: 16,
+    matches: ["Will", "Will", "William"],
+  },
+  {
+    bookIndex: 7,
+    chapterIndex: 19,
+    matches: ["Will", "Will", "Will", "Will", "Will"],
+  },
+  {
+    bookIndex: 8,
+    chapterIndex: 5,
+    matches: ["Will", "Will", "Will"],
+  },
+  {
+    bookIndex: 8,
+    chapterIndex: 8,
+    matches: ["Will"],
+  },
+  {
+    bookIndex: 8,
+    chapterIndex: 10,
+    matches: ["Will"],
+  },
+];
---
+++ src/stores/QueryStore.ts
@@ -0,0 +1,29 @@
+import { create } from "zustand";
+import { Will } from "../data/querys/positions_Will";
+import { Test } from "../data/querys/positions_test";
+
+type Querys = "Will" | "Test" | "";
+
+export interface BookPosition {
+  bookIndex: number;
+  chapterIndex: number;
+  matches?: string[];
+}
+
+export const queryRefs: Record<string, BookPosition[]> = {
+  Will: Will,
+  Test: Test,
+  default: [],
+};
+
+export interface Query {
+  query: Querys;
+  chooseQuery: (query: Querys) => void;
+}
+
+export const useQuery = create<Query>((set) => ({
+  query: "",
+  chooseQuery: (query: Querys) => {
+    set({ query });
+  },
+}));
