72d5b8e79107a4e1175db9cc3a1d4564b2c53015
Manonisch <e.baudisch@mailbox.org>
2025-05-10 11:03:03 +0200

    icons?

---
+++ src/assets/porridge.tsx
@@ -0,0 +1,22 @@
+import { SVGProps } from "react"
+
+export const PorridgeIcon = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      width="100%"
+      height="100%"
+      viewBox="0 0 121 113"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      xmlSpace="preserve"
+      style={{ fillRule: 'evenodd', clipRule: 'evenodd', strokeLinejoin: 'round', strokeMiterlimit: '2' }}
+      fill="currentColor"
+      {...props}
+    >
+      <g>
+        <path d="M0,48c0,-0 0.098,60 40,60c2.544,-0 0,5 0,5l40,0c-0,0 -2.275,-5 -0,-5c40.398,-0 40,-60 40,-60l-12.664,-0c-0,-0 -2.999,-10.78 -7.336,-13.51c-11.997,-3.443 -14.293,-11.229 -20,-12.11c-5.202,-0.804 -14.244,6.818 -14.244,6.818c0,0 -6.445,-7.312 -10.899,-8.433c-2.537,-0.639 -5.432,0.995 -9.857,1.235c-4.849,0.263 -8.27,-2.031 -11.135,-0.832c-2.865,1.2 -6.055,8.03 -6.055,8.03c-0,0 -11.102,-1.763 -14.129,0c-3.028,1.764 -3.028,7.45 -4.037,10.583c-0.865,2.685 -2.019,8.219 -2.019,8.219l-7.625,-0Z" />
+        <path d="M89,28.648l14,-28.648l4,2l-15,29" />
+      </g>
+    </svg>)
+}
