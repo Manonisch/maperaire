0d2480a5b22fbf4ac222e87f6865b35a5476cf0a
Gilbert Röhrbein <gilbert@innovailable.eu>
2025-05-10 12:42:26 +0200

    iconssss

---
+++ src/assets/nuts.tsx
@@ -0,0 +1,19 @@
+import { SVGProps } from "react"
+
+export const NutsIcon = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      width="100%"
+      height="100%"
+      viewBox="0 0 342 318"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      xmlSpace="preserve"
+      style={{ fillRule: 'evenodd', clipRule: 'evenodd', strokeLinejoin: 'round', strokeMiterlimit: '2' }}
+      fill="currentColor"
+      {...props}
+    >
+      <path d="M66.578,4.586c18.154,14.357 43.237,8.208 43.237,8.208c0,0 132.455,-54.795 214.342,62.413c21.083,30.177 25.415,81.818 2.233,110.413c-32.382,39.943 -147.02,115.333 -196.523,129.24c-2.742,0.771 -5.542,1.333 -8.385,1.701c-33.899,4.39 -73.802,-18.886 -92.109,-47.494c-79.267,-123.87 27.008,-221.133 27.008,-221.133c0,0 18.045,-18.168 10.197,-43.348Zm260.684,163.74c-0.574,-0.004 -33.314,-35.738 -136.04,29.441c-96.427,61.184 -70.949,103.47 -72.504,104.331c8.829,7.784 163.413,-74.357 208.544,-133.772Z" />
+    </svg>)
+}
